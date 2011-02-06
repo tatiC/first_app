26f502f7f48164ffe3abf272aced49c3900bd400
tati Carvalho <tatigmc@yahoo.com.br>
2011-02-05 23:30:11 -0500

    adicionando estrutura inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ab9bda9a71ecc7437ab6c9a605afae7afd998fca1f17fd9d76fa0c5dd502dad046edf5fc36263e1fe62c232a8f6f3219fb73cf83ce407759307754da7c7bb7fe'
